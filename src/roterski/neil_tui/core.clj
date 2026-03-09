e8d0b048a75d88c9d7a930c21494584512e90519
Piotr Roterski <pt.roterski@gmail.com>
2026-03-09 21:33:12 +0100

    adjust the script so it can be installed as bbin

--- src/roterski/neil_tui/core.clj
+++ src/roterski/neil_tui/core.clj
@@ -39,19 +39,23 @@
                                       :continue)})
     @!files))
 
-(when (tty? :stdout)
-  (let [{:keys [lib]} (->> (autocomplete (fn [query]
-                                           (when (> (count query) 3)
-                                             (let [{:keys [exit out]} @(bp/process {:out :string
-                                                                                    :err :string}
-                                                                                   (str "neil dep search " query))]
-                                               (when (zero? exit)
-                                                 (str/split-lines out))))))
-                           neil-line->map)
-        deps-edn-paths (->deps-edn-paths)
-        deps-edn-path (if (= (count deps-edn-paths) 1)
-                        (first deps-edn-paths)
-                        (autocomplete (fn [query]
-                                        (->> deps-edn-paths
-                                             (filter #(str/includes? % query))))))]
-    (bp/shell (str "neil add dep " lib " --deps-file " deps-edn-path))))
+(defn -main [& _args]
+  (when (tty? :stdout)
+    (let [{:keys [lib]} (->> (autocomplete (fn [query]
+                                             (when (> (count query) 3)
+                                               (let [{:keys [exit out]} @(bp/process {:out :string
+                                                                                      :err :string}
+                                                                                     (str "neil dep search " query))]
+                                                 (when (zero? exit)
+                                                   (str/split-lines out))))))
+                             neil-line->map)
+          deps-edn-paths (->deps-edn-paths)
+          deps-edn-path (if (= (count deps-edn-paths) 1)
+                          (first deps-edn-paths)
+                          (autocomplete (fn [query]
+                                          (->> deps-edn-paths
+                                               (filter #(str/includes? % query))))))]
+      (bp/shell (str "neil add dep " lib " --deps-file " deps-edn-path)))))
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (-main))
